c2c19e524644fecc7ac34eb4d6cd1cb49615dffb
Daniele-Dionisio <Danieledionisio34@gmail.com>
2026-09-08 00:20:01 +0000

    Segunda rota criada

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const app = express();
 const PORT = 3000;
 
 let produtos = [
-    {id:1, nome: "monitor"},
-    {id:2, nome: "teclado"}
+    {id:1, nome: "monitor", categoria: "eletronicos"},
+    {id:2, nome: "teclado", categoria: "eletronicos"}
 ]
 
 app.get('/produtos/id', (req,res) => {
@@ -17,4 +17,17 @@ app.get('/produtos/id', (req,res) => {
         return res.status(404).json({error:'Produto não encontrado!'});
     }
     res.status(200).json(produto)
+});
+
+app.get('/produtos', (req, res) => {
+    const categoria = req.query.categoria;
+
+    const resultado = produtos.filter(
+        produto => produto.categoria == categoria
+    );
+    res.json(resultado);
+});
+
+app.listen(porta, () => {
+    console.log(`Servidor rodando em http://localhost:${porta}`);
 });
